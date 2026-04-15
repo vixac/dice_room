aee3c7c499d117128d00472dffdb6d851fc961a4
vic <vic@vixac.com>
2026-04-15 18:50:44 +0100

    bullet store

--- main.go
+++ main.go
@@ -2,51 +2,68 @@ package main
 
 import (
 	"dice_room/store"
+	"dice_room/store/bullet_store"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
-	sqlite_store "github.com/vixac/bullet/store/sqlite"
 	"github.com/vixac/bullet/store/store_interface"
+
 	"github.com/vixac/firbolg_clients/bullet/local_bullet"
+	"github.com/vixac/firbolg_clients/bullet/rest_bullet"
 )
+
+func buildMemoryStore() *store.MemoryStore {
+	fmt.Printf("Building memory store")
+	return store.NewMemoryStore()
+}
+
+func buildBullet(bulletPort int) store.Store {
+	fmt.Printf("Building bullet local")
+	space := store_interface.TenancySpace{
+		AppId:     5000,
+		TenancyId: 0,
+	}
+
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	option := rest_bullet.WithLogger(logger)
+
+	bulletStr := strconv.Itoa(bulletPort)
+
+	restClient := rest_bullet.NewRestClient("http://localhost:"+bulletStr, space, option)
+
+	fmt.Printf("VX: rest client %s\n", restClient.AppId)
+
+	store := bullet_store.NewBulletStore(restClient)
+	return store
+}
 
 func main() {
 
 	space := store_interface.TenancySpace{
-		AppId:     123,
+		AppId:     1234,
 		TenancyId: 0,
 	}
-	path := os.Getenv("GOT_BOLT")
-	if path == "" {
-		log.Fatal("missing env GOT_BOLT, which should be the path to the got bolt file")
-	}
-
-	sqlitePath := os.Getenv("GOT_SQLITE")
-	if sqlitePath == "" {
-		log.Fatal("missing env GOT_SQLITE, which should be the path to the got sqlite file")
-	}
-	sqlite, err := sqlite_store.NewSQLiteStore(sqlitePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	localBullet := local_bullet.LocalBullet{
-		Space: space,
-		Store: sqlite,
-	}
-
-	fmt.Println("Dice Room begins... %+v\n", localBullet)
+	fmt.Printf("Dice Room begins...\n")
 	args, err := ReadArgs()
 	if err != nil {
 		log.Fatal("Error parsing args: ", err)
 	}
 
-	store := store.NewMemoryStore()
-	//store := bullet_store.NewBulletStore(&localBullet)
+	localBullet := local_bullet.LocalBullet{
+		Space: space,
+	}
+	fmt.Printf("VX:local bullet %s\n", localBullet.Space)
+
+	//ene, err := grove_engine.NewGroveEngine(&localBullet)
+	//ene, err := grove_engine.NewGroveEngine(restClient)
+
+	//
 	broadcaster := NewBroadcaster()
+
+	store := buildBullet(args.BulletPort)
 	srv := NewServer(store, broadcaster, args.HostPrefix)
 
 	addr := ":" + strconv.Itoa(args.Port)
